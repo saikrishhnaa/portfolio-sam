31e817b08728ad161c4335c07fcf0281f5cd77ff
saikrishhnaa <saikrishnaamakam@gmail.com>
2024-01-14 19:46:02 +0530

    Done with the initial setup

---
+++ netflix-clone/src/App.tsx
@@ -0,0 +1,23 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+function AppRouter() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        path="/"
+        element={<h1 className="text-3xl font-bold underline">Hello World!</h1>}
+      />,
+    ),
+  );
+
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default function App() {
+  return <AppRouter />;
+}
