d12cfaf28be9ca1d9118bedeb3b09cb10606de4b
saikrishhnaa <saikrishnaamakam@gmail.com>
2024-02-11 04:11:13 +0530

    Setup the project

--- netflix-clone/tailwind.config.js
+++ netflix-clone/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "hsl(0deg 0% 8%)",
+        netflixRed: "#e50a14",
+      },
+      animation: {
+        "slide-rtl": "slide-rtl .4s ease-in-out",
+      },
+      keyframes: {
+        "slide-rtl": {
+          from: { "margin-right": "-90%" },
+          to: { "margin-right": "0%" },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
